b29512326b10cb43db78358fc15a490669e7eb3f
Elliott <elliottrecord@gmail.com>
2018-12-02 13:54:21 +0000

    Used tags on objects to print different behavior.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -24,6 +24,19 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Friendly.");
+                break;
+            default:
+                print("You died.");
+                break;
+        }
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
